5614fb43727220844c80765d702ac26c0ba9e813
Severin407 <severin-wagemann@web.de>
2016-10-20 10:34:38 +0200

    Shitty php is shitty

--- PHP/grohssack.php
+++ PHP/grohssack.php
@@ -1,4 +1,10 @@
 <?php
-	if (!$_GET['area'])
-		echo "whatevs";
+$file = 'boring.txt';
+// Öffnet die Datei, um den vorhandenen Inhalt zu laden
+fopen($file, 'rw');
+$current = file_get_contents($file);
+// Fügt eine neue Person zur Datei hinzu
+$current .= '\''.$_GET['area'].'\';\''.$_GET['height'].'\';\''.$_GET['floors'].'\';\''.$_GET['age']."'\n";
+// Schreibt den Inhalt in die Datei zurück
+file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
 ?>
